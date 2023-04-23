214e1f1495e8ac310b1b004b5cfec938956b1371
Juan Gouveia <85375414+jcfgDEV@users.noreply.github.com>
2023-04-23 01:53:09 +0100

    new changes

--- app/layout.js
+++ app/layout.js
@@ -1,55 +1,54 @@
-'use client'
+
 import './globals.css'
 import Logo from './components/Logo.js'
-import GoogleAnalytics from './components/GoogleAnalytics.js';
-import CookieBanner from './components/CookieBanner';
 
 
-// export const metadata = {
-//     title: "Blog John's Tech",
-//     description: 'Blog para tips y pequeños modulos de codigo y algunas cosas interesante',
-//     type: 'website',
-//     robots: {
-//         index: true,
-//     },
-//     icons: {
-//         icon: '/android-chrome-512x512.png',
-//         shortcut: '/favicon-32x32.png',
-//         apple: '/apple-touch-icon.png',
-//     },
-//     viewport: {
-//         width: 'device-width',
-//         initialScale: 1,
-//         maximumScale: 1,
-//     },
-//     openGraph: {
-//         title: "Blog John's Tech",
-//         description: 'Blog para tips y pequeños modulos de codigo y algunas cosas interesante',
-//         url: 'https://www.codegeekery.com/',
-//         siteName: 'Blog John',
-//         image: 'https://i.postimg.cc/HnGKQ1rk/grid-0-1.png',
-//         images: [
-//             {
-//                 url: 'https://i.postimg.cc/HnGKQ1rk/grid-0-1.png',
-//                 secureUrl: 'https://i.postimg.cc/HnGKQ1rk/grid-0-1.png',
-//                 alt: 'Logo Website Blog Tech',
-//                 width: 1200,
-//                 height: 630,
-//                 type: 'image/png',
-//             },
-//         ],
-//         locale: 'en-US',
-//         type: 'website',
-//     },
-//     twitter: {
-//         card: 'summary_large_image',
-//         title: 'Logo Website Blog Tech',
-//         description:'Blog para tips y pequeños modulos de codigo y algunas cosas interesante',
-//         creator: '@jcfgDEV',
-//         images: ['https://i.postimg.cc/HnGKQ1rk/grid-0-1.png'],
-//     },
 
-// }
+export const metadata = {
+    title: "Blog John's Tech",
+    description: 'Blog para tips y pequeños modulos de codigo y algunas cosas interesante',
+    type: 'website',
+    robots: {
+        index: true,
+    },
+    icons: {
+        icon: '/android-chrome-512x512.png',
+        shortcut: '/favicon-32x32.png',
+        apple: '/apple-touch-icon.png',
+    },
+    viewport: {
+        width: 'device-width',
+        initialScale: 1,
+        maximumScale: 1,
+    },
+    openGraph: {
+        title: "Blog John's Tech",
+        description: 'Blog para tips y pequeños modulos de codigo y algunas cosas interesante',
+        url: 'https://www.codegeekery.com/',
+        siteName: 'Blog John',
+        image: 'https://i.postimg.cc/HnGKQ1rk/grid-0-1.png',
+        images: [
+            {
+                url: 'https://i.postimg.cc/HnGKQ1rk/grid-0-1.png',
+                secureUrl: 'https://i.postimg.cc/HnGKQ1rk/grid-0-1.png',
+                alt: 'Logo Website Blog Tech',
+                width: 1200,
+                height: 630,
+                type: 'image/png',
+            },
+        ],
+        locale: 'en-US',
+        type: 'website',
+    },
+    twitter: {
+        card: 'summary_large_image',
+        title: 'Logo Website Blog Tech',
+        description:'Blog para tips y pequeños modulos de codigo y algunas cosas interesante',
+        creator: '@jcfgDEV',
+        images: ['https://i.postimg.cc/HnGKQ1rk/grid-0-1.png'],
+    },
+
+}
 
 
 
@@ -58,8 +57,8 @@ export default function RootLayout({ children }) {
     return (
         <>
             <html lang="en">
-            <GoogleAnalytics GA_MEASUREMENT_ID='G-SB4L1MWSTR'/>
                 <body>
+                    
                     <Logo />
                     {children}
                     <CookieBanner/>
